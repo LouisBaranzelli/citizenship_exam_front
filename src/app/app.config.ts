cd3eee3fbe5ee9a3aca1c250e6321fea2f74f9da
Louis <contact@louisbaranzelli.com>
2026-01-20 15:27:43 +0100

    [LANGUE]: Ajout des langues

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,23 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideTranslateService, TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {
+  provideTranslateHttpLoader,
+
+} from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideTranslateService({
+      lang: 'fr',
+      fallbackLang: 'en',
+      loader: provideTranslateHttpLoader({
+        prefix: './assets/i18n/',
+        suffix: '.json'
+      })
+    })
   ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient} from '@angular/common/http';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader();
+}
