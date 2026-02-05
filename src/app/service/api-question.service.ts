36d474b9811c73a84f9a3c6225eb44cf81bea37c
Louis <contact@louisbaranzelli.com>
2026-02-05 14:42:13 +0100

    [API SERVICE] feat confirme la fonctionnalité de la langue lors du sxitxh des de langue des questions

--- src/app/service/api-question.service.ts
+++ src/app/service/api-question.service.ts
@@ -14,72 +14,25 @@ export class ApiQuestionService {
 
   public mapRunningOutOfQuestion =  signal<Map<string, boolean>>(new Map<string, boolean>())
 
-  private question1: Question = {
-    id: 0,
-    question: "Quelle est la capital de la france ?",
-    answers: [{id: 0, answer:"Oui", correct:true},
-            {id: 1, answer:"Non", correct:false},
-            {id: 2, answer:"Peut-être", correct:false},
-            {id: 3, answer:"bien-sûr", correct:false},
-            ],
-    language: DEFAULT_LANGUAGE,
-    theme: "T1",
-    level: "L1",
-  }
 
-  private question2: Question = {
-    id: 2,
-    question: "Quel est le plus grand pays ?",
-    answers: [{id: 0, answer:"Grand", correct:true},
-      {id: 1, answer:"petit", correct:false},
-      {id: 2, answer:"moyen", correct:false},
-    ],
-    language: DEFAULT_LANGUAGE,
-    theme: "T1",
-    level: "L1",
-  }
-  // private question3: Question = {
-  //   id: 3,
-  //   label: "Comment t'appeles tu ? ",
-  //   answers: [],
-  //   language: DEFAULT_LANGUAGE
-  // }
-  private question4: Question = {
-    id: 4,
-    question: "Quel est le nom de notre planète ?",
-    answers: [{id: 0, answer:"Oui", correct:true},
-      {id: 1, answer:"Non", correct:false}
-    ],
-    language: DEFAULT_LANGUAGE,
-    theme: "T1",
-    level: "L1",
-
-  }
-  // private question5: Question = {
-  //   id: 5,
-  //   label: "Ou es tu né ?",
-  //   answers: [],
-  //   language: DEFAULT_LANGUAGE
-  // }
-  private questions: Question[];
   private httpClient: HttpClient
 
   constructor(httpClient: HttpClient) {
-    this.questions = [this.question1, this.question2, this.question4];
     this.httpClient = httpClient
   }
 
 
-
-
   public async fetchQuestion(id: number, language: Language): Promise<Question | null> {
-    let question = this.getRandomQuestion()
-    question.id = id
-    console.log("Generate random question to simulate api: " + question.question)
-    return new Promise(resolve => {
-      setTimeout(() => resolve(question), 500)
-    })
-  }
+
+    try {
+      console.info(environment.apiUrl + "/questions/" + language.id.slice(-2) + "/" + id)
+      const question: Question = await firstValueFrom(this.httpClient.get<Question>(environment.apiUrl + "/questions/" + language.id.slice(-2) + "/" + id))
+      return question
+    } catch (error){
+      console.error(error)
+      }
+      return null
+    }
 
   public async fetchRandomQuestion(level: Level, theme: Theme, language: Language, alreadyAskedQuestions: number[]): Promise<Question | null> {
 
@@ -97,13 +50,6 @@ export class ApiQuestionService {
     }
   }
 
-  getRandomQuestion(): Question{
-
-    const randomIndex = Math.floor(Math.random() * this.questions.length);
-    const randomQuestion = this.questions[randomIndex];
-    return randomQuestion
-
-  }
 
   public test() {
     this.httpClient.get(environment.apiUrl + "/questions/test").subscribe(res => console.log(res))
